96f9eb945ed33703b37e9b268a29fcfe3c2f1838
Youssef Alshaarawi <youssefalshaarawi@yahoo.com>
2020-03-15 16:32:14 +0200

    time chart

---
+++ src/eg/edu/alexu/csd/filestructure/timeanalytics/DataGeneration.java
@@ -0,0 +1,63 @@
+package eg.edu.alexu.csd.filestructure.timeanalytics;
+
+import com.sun.deploy.net.MessageHeader;
+import eg.edu.alexu.csd.filestructure.sort.Sort;
+import eg.edu.alexu.csd.filestructure.sort.SortingAlgorithms.BubbleSort;
+import eg.edu.alexu.csd.filestructure.sort.SortingAlgorithms.QuickSort;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class DataGeneration {
+
+    private static ArrayList<Integer> array=new ArrayList<>();
+
+    public static ArrayList<Integer> getN2time() {
+        return n2time;
+    }
+
+    public static ArrayList<Integer> getNlongntime() {
+        return nlongntime;
+    }
+
+    public static ArrayList<Integer> getHeaptime() {
+        return heaptime;
+    }
+
+    private static ArrayList<Integer> n2time=new ArrayList<>();
+    private static ArrayList<Integer> nlongntime=new ArrayList<>();
+    private static ArrayList<Integer> heaptime=new ArrayList<>();
+
+    private static int n=1000;
+    public static void generateData() {
+        Random r = new Random();
+        Sort sort=new Sort<>();
+        for(int i=0;i<n;i++)
+        {
+            int val = r.nextInt(2147483647);
+            array.add(val);
+            Instant start = Instant.now();
+            ArrayList<Integer> temp=new ArrayList<>(array);
+            sort.sortSlow(temp);
+            Instant finish = Instant.now();
+            long timeElapsed = Duration.between(start, finish).toMillis();  //in m
+            n2time.add((int) timeElapsed);
+             start = Instant.now();
+             temp= new ArrayList<>(array);
+            sort.heapSort(temp);
+             finish = Instant.now();
+             timeElapsed = Duration.between(start, finish).toMillis();  //in m
+            heaptime.add((int) timeElapsed);
+            start = Instant.now();
+             temp=new ArrayList<>(array);
+            sort.sortFast(temp);
+             finish = Instant.now();
+             timeElapsed = Duration.between(start, finish).toMillis();  //in m
+            nlongntime.add((int) timeElapsed);
+        }
+
+    }
+
+}
---
+++ src/eg/edu/alexu/csd/filestructure/timeanalytics/TimeChart.java
@@ -0,0 +1,76 @@
+package eg.edu.alexu.csd.filestructure.timeanalytics;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
+public class TimeChart extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        HBox root=new HBox();
+        Scene scene=new Scene(root,1200,700);
+        NumberAxis xAis=new NumberAxis();
+        xAis.setLabel("Array Size(n)");
+        NumberAxis yAxis=new NumberAxis();
+        yAxis.setLabel("RunTime(ms)");
+        yAxis.setLowerBound(0.0001);
+        NumberAxis xAis2=new NumberAxis();
+        xAis2.setLabel("Array Size(n)");
+        NumberAxis yAxis2=new NumberAxis();
+        yAxis2.setLabel("RunTime(ms)");
+        yAxis2.setLowerBound(0.0001);
+        NumberAxis xAis3=new NumberAxis();
+        xAis3.setLabel("Array Size(n)");
+        NumberAxis yAxis3=new NumberAxis();
+        yAxis3.setLabel("RunTime(ms)");
+        yAxis3.setLowerBound(0.0001);
+        LineChart<Number,Number> lineChart1=new LineChart<Number, Number>(xAis,yAxis);
+        LineChart<Number,Number> lineChart2=new LineChart<Number, Number>(xAis2,yAxis2);
+        LineChart<Number,Number> lineChart3=new LineChart<Number, Number>(xAis3,yAxis3);
+        lineChart1.setTitle("Comparison between sorting algorithms");
+        lineChart2.setTitle("Comparison between sorting algorithms");
+        lineChart3.setTitle("Comparison between sorting algorithms");
+        DataGeneration dataGeneration=new DataGeneration();
+        DataGeneration.generateData();
+        XYChart.Series<Number,Number> data1=new XYChart.Series<>();
+        XYChart.Series<Number,Number> data2=new XYChart.Series<>();
+        XYChart.Series<Number,Number> data3=new XYChart.Series<>();
+        ArrayList<Integer> n2time=dataGeneration.getN2time();
+        ArrayList<Integer> nlogn= DataGeneration.getNlongntime();
+        ArrayList<Integer> heaptime=dataGeneration.getHeaptime();
+
+        data1.setName("Slow Sort<Bubble Sort>");
+        data2.setName("Fast Sort<Quick Sort>");
+        data3.setName("Heap Sort");
+
+        for(int i=0;i<n2time.size();i=i+50) {
+            data1.getData().add(new XYChart.Data<Number, Number>(i, n2time.get(i)));
+            data2.getData().add(new XYChart.Data<Number, Number>(i, nlogn.get(i)));
+            data3.getData().add(new XYChart.Data<Number, Number>(i, heaptime.get(i)));
+        }
+        lineChart1.autosize();
+        lineChart2.autosize();
+        lineChart3.autosize();
+        lineChart1.getData().add(data1);
+        lineChart2.getData().add(data2);
+        lineChart3.getData().add(data3);
+        root.getChildren().addAll(lineChart1,lineChart2,lineChart3);
+
+        primaryStage.setTitle("Sorting Algorithms Complexity");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
