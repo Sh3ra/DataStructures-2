f96e5202bd3712e9e957b5d69bf9240df5ae01c5
sh3ra <youssefalshaarawi@yahoo.com>
2020-04-11 21:18:19 +0200

    rbt.java before search

--- RedBlackTree/src/eg/edu/alexu/csd/filestructure/redblacktree/RedBlackTree.java
+++ RedBlackTree/src/eg/edu/alexu/csd/filestructure/redblacktree/RedBlackTree.java
@@ -1,30 +1,26 @@
 package eg.edu.alexu.csd.filestructure.redblacktree;
 
 public class RedBlackTree implements IRedBlackTree {
+    INode root=null;
     @Override
-    public INode getRoot() {
-        return null;
-    }
+    public INode getRoot() { return root; }
 
     @Override
-    public boolean isEmpty() {
-        return false;
-    }
+    public boolean isEmpty() { return root==null; }
 
     @Override
     public void clear() {
-
+        root=null;
     }
 
     @Override
     public Object search(Comparable key) {
+
         return null;
     }
 
     @Override
-    public boolean contains(Comparable key) {
-        return false;
-    }
+    public boolean contains(Comparable key) { return search(key)!=null; }
 
     @Override
     public void insert(Comparable key, Object value) {
